30ae0497246a408703362fb98b9dbc25af60d710
Hiroki Horiuchi <horiuchi.g@gmail.com>
2020-05-03 01:15:18 +0900

    feat: add function for fixing up the project name

--- src/create.ts
+++ src/create.ts
@@ -1,37 +1,59 @@
 import commander from 'commander';
 import validate from 'validate-npm-package-name';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('../package.json');
-
 let projectName: string | undefined;
 
-const program = new commander.Command(packageJson.name)
-    .version(packageJson.version)
-    .arguments('<project-name>')
-    .usage('<project-name> [options]')
-    .action((name) => {
-        projectName = name;
-    })
-    .option('--verbose', 'print additional logs')
-    .parse(process.argv);
+function parseArgs(): commander.Command {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const packageJson = require('../package.json');
 
-if (projectName == null) {
-    console.log(program.helpInformation());
-    process.exit(1);
-}
-const result = validate(projectName);
-if (!result.validForNewPackages) {
-    console.error('<project-name> is invalid.');
-    if (result.errors != null) {
-        result.errors.forEach((e) => console.error('  ' + e));
-    }
-    if (result.warnings != null) {
-        result.warnings.forEach((w) => console.warn('  ' + w));
-    }
-    process.exit(1);
+    return new commander.Command(packageJson.name)
+        .version(packageJson.version)
+        .arguments('<project-name>')
+        .usage('<project-name> [options]')
+        .action((name) => {
+            projectName = name;
+        })
+        .option('--verbose', 'print additional logs')
+        .parse(process.argv);
 }
 
+function addPrefix(name: string): string {
+    const m = /^(?<prefix>@?dtsgenerator(?:-|\/)?)?(?<content>.+)$/.exec(name);
+    if (m == null) {
+        console.error('What!?');
+        process.exit(255);
+    }
+    const prefix = m.groups?.['prefix'];
+    const content = m.groups?.['content'] || '';
+    if (prefix != null && prefix[0] === '@') {
+        return '@dtsgenerator/' + content;
+    }
+    return 'dtsgenerator-' + content;
+}
+function checkProjectName(name: string | undefined): string {
+    if (name == null) {
+        console.log(program.helpInformation());
+        process.exit(1);
+    }
+    name = addPrefix(name);
+    const result = validate(name);
+    if (!result.validForNewPackages) {
+        console.error(`<project-name>(${name}) is invalid.`);
+        if (result.errors != null) {
+            result.errors.forEach((e) => console.error('  ' + e));
+        }
+        if (result.warnings != null) {
+            result.warnings.forEach((w) => console.warn('  ' + w));
+        }
+        process.exit(1);
+    }
+    return name;
+}
+
+const program = parseArgs();
 const verbose: boolean = program.verbose;
+
+projectName = checkProjectName(projectName);
 
 console.log(`Result: ${projectName} ${verbose ? '[verbose]' : ''}`);
